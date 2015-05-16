40cdb9869664b9a1a53563bc2264fde4ed1f9dce
Henrik Paul <henrik.paul@gmail.com>
2015-05-17 02:08:38 +0300

    Split tests into two files

--- test/parse.js
+++ test/parse.js
@@ -3,166 +3,44 @@ var parse = require("../lib/parser.js");
 var util = require("../lib/util.js");
 var assertIsString = util.assertIsString;
 
-describe('A variable', function() {
-	it('should be itself if in quoted format', function() {
+describe('A variable', function () {
+	it('should be itself if in quoted format', function () {
 		var result = parse("{{ \"foo\" }}");
-		assert.equal("foo", result);
+		assert.equal(result, "foo");
         assertIsString(result);
 	});
 	
 	it('should cope with an escaped quote', function() {
-		assert.equal("\"", parse("{{ \"\\\"\" }}"));
+		assert.equal(parse("{{ \"\\\"\" }}"), "\"");
 	});
 
     it('should take its value from the context', function() {
-		assert.equal("1", parse("{{ number }}", { 'number': 1 }));
+		assert.equal(parse("{{ number }}", { 'number': 1 }), "1");
+	});
+	
+	it('should fail variable is not in context', function() {
+		assert.throws(function() {
+			parse("{{ foo }}");
+		});
 	});
 	
 	describe('with lowercase filter', function() {
 		it('should be lowercase', function() {
-			assert.equal("foo", parse("{{ \"Foo\"|lowercase }}"));
-		});
-		
-		it('should not accept numbers', function() {
-			try {
-				parse("{{ var|lowercase }}", {'var': 1});
-				assert.fail();
-			} catch (e) {
-				// expected
-			}
-		});
-		
-		it('should not accept functions', function() {
-			try {
-				parse("{{ var|lowercase }}", {'var': function() {}});
-				assert.fail();
-			} catch (e) {
-				// expected
-			}
+			assert.equal(parse("{{ \"Foo\"|lowercase }}"), "foo");
 		});
 	});
 	
 	describe('with uppercase filter', function() {
 		it('should be uppercase', function() {
-			assert.equal("FOO", parse("{{ \"Foo\"|uppercase }}"));
-		});
-		
-		it('should not accept numbers', function() {
-			try {
-				parse("{{ var|uppercase }}", {'var': 1});
-				assert.fail();
-			} catch (e) {
-				// expected
-			}
-		});
-		
-		it('should not accept functions', function() {
-			try {
-				parse("{{ var|uppercase }}", {'var': function() {}});
-				assert.fail();
-			} catch (e) {
-				// expected
-			}
+			assert.equal(parse("{{ \"Foo\"|uppercase }}"), "FOO");
 		});
 	});
 	
 	describe('with a filter that doesn\'t exist', function() {
 		it('should fail', function() {
-			try {
+			assert.throws(function() {
 				parse("{{ \"foo\"|foobar }}");
-				assert.fail();
-			} catch (e) {
-				// expected
-			} 
-		});
-	});
-});
-
-var acceptsOnly = function(accepted, f) {
-	util.assertIsFunction(f);
-	util.assertIsString(accepted);
-	
-	var only = function(accepted, trying, f, arg) {
-		var _it = function(desc, f, arg) {
-			it(desc, function(done) {
-				f(arg);
-				done();
 			});
-		};
-		
-		var _but = function(desc, f, arg) {
-			it(desc, function(done) {
-				try {
-					f(arg);
-					assert.fail();
-				} catch (e) {
-					// expected
-					done();
-				}
-			});
-		};
-		
-		if (accepted === trying) {
-			_it("accepts "+trying, f, arg);
-		} else {
-			_but("doesn't accept "+trying, f, arg);
-		}
-	};
-	
-	only(accepted, "strings", f, "string");
-	only(accepted, "functions", f, function() {});
-	only(accepted, "numbers", f, 1);
-	only(accepted, "objects", f, {});
-	only(accepted, "booleans", f, true);
-	only(accepted, "nulls", f, null);
-	only(accepted, "undefineds", f);
-	only(accepted, "arrays", f, []);
-	only(accepted, "array-likes", f, {0: true, 1:true});
-};
-
-describe("Util", function() {
-	describe(".assertIsFunction", function() {
-		acceptsOnly("functions", util.assertIsFunction);
-	});
-	describe(".assertIsString", function() {
-		acceptsOnly("strings", util.assertIsString);
-	});
-	describe(".assertIsArray", function() {
-		acceptsOnly("arrays", util.assertIsArray);
-	});
-	describe(".assertIsNumber", function() {
-		acceptsOnly("numbers", util.assertIsNumber);
-	});
-	
-	describe(".splitContainingQuotedStrings", function() {
-		var split = util.splitContainingQuotedStrings;
-		it("returns itself arrayed if nothing is split", function() {
-			assert.deepEqual(["string"], split("string", "."));
-		});
-		
-		it("splits as expected without quotes", function() {
-			assert.deepEqual(["hello", "world"], split("hello world", " "));
-		});
-		
-		it("doesn't choke on quotes without a split", function() {
-			assert.deepEqual(
-				["hello", "\"world\""],
-				split("hello \"world\"", " ")
-			);
-		});
-		
-		it("keeps quoted sections togehter", function() {
-			assert.deepEqual(
-				["hello", "\"Hello World\"", "world"],
-				split("hello \"Hello World\" world", " ")
-			);
-		});
-		
-		it("works with two quoted sections", function() {
-			assert.deepEqual(
-				["\"hello world\"", "\"hello world\""],
-				split("\"hello world\" \"hello world\"", " ")
-			);
 		});
 	});
 });
