630eb07fb68d6b2a86429312d80444b12bb9398e
Henrik Paul <henrik.paul@gmail.com>
2015-06-17 00:55:43 +0300

    First naive implementation of dirdiff
    This version only checks for same relative path structure

---
+++ test/dirdiff.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Henrik Paul
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var assert = require('assert');
+var diff = require('../lib/dirdiff.js');
+var path = require('path');
+
+var pathDirdff = __dirname + path.sep + 'dirdiff';
+var pathA = pathDirdff + path.sep + 'a';
+var pathB = pathDirdff + path.sep + 'b';
+
+describe('dirdiff', function() {
+  var assertNoDifferences = function(done) {
+    return function(common, missing, extra) {
+      assert.ok(common.length > 0);
+      assert.ok(missing.length === 0);
+      assert.ok(extra.length === 0);
+      done();
+    };
+  };
+
+  it('works when source and target are the same', function(done) {
+    diff(__dirname, __dirname, assertNoDifferences(done));
+  });
+
+  it('correctly detects no file-level differences', function(done) {
+    diff(pathA, pathB, assertNoDifferences(done));
+  });
+});
